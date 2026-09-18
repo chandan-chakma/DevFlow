4ff45ff6eb5d0281e03719d5175ab6db2a05e7da
chandan-chakma <cchakma19@gmail.com>
2026-09-18 16:44:23 +0800

    footer

--- src/pages/SharedPages/Footer/Footer.jsx
+++ src/pages/SharedPages/Footer/Footer.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import Logo from '../../../Components/Logo/Logo.jsx';
+import { FaFacebookSquare, FaLinkedin, FaTwitterSquare, FaYoutubeSquare } from 'react-icons/fa';
+import { FaSquareGithub } from 'react-icons/fa6';
 
 const Footer = () => {
     return (
         <div>
-            <h1>FOtter</h1>
+            <footer className="footer sm:footer-horizontal bg-base-100 items-center justify-between py-4 px-8">
+                <aside className="grid-flow-col items-center gap-10">
+                    <Logo></Logo>
+                    <nav class="grid grid-flow-col gap-4 text-muted">
+                        <a href="#features">Features</a>
+                        <a href="#pricing">Pricing</a>
+                        <a href="#about">About</a>
+                        <a href="#contact">Contact</a>
+                    </nav>
+                </aside>
+                
+                <div className='flex flex-col'>
+                    <nav className="flex gap-3">
+                        <a>
+                            <FaSquareGithub className='w-6 h-6' />
+                        </a>
+                        <a>
+                            <FaLinkedin className='w-6 h-6' />
+                        </a>
+                        <a>
+                            <FaTwitterSquare className='w-6 h-6' />
+                        </a>
+                        <a>
+                            <FaFacebookSquare className='w-6 h-6' />
+                        </a>
+                        <a>
+                            <FaYoutubeSquare className='w-6 h-6' />
+                        </a>
+                      
+
+                    </nav>
+
+                    <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+
+               </div>
+           
+            </footer>
         </div>
     );
 };
